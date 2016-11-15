ff96970d25aaee4e15a0eeceafcfa259071d4955
Stan Zhai <zhaishidan@haizhi.com>
2016-11-15 14:45:16 +0800

    first commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "jvm-exercise"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+    
---
+++ src/main/scala/cn/zhaishidan/exercise/thread/Main.scala
@@ -0,0 +1,12 @@
+package cn.zhaishidan.exercise.thread
+
+/**
+  * Created by stan on 2016/11/15.
+  */
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    println("test")
+  }
+
+}
