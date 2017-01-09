16f9969e34827e465624b6fe52fc485877e2c8d9
stanzhai <stanzhai@outlook.com>
2017-01-09 10:02:49 +0800

    add serialization

---
+++ src/main/scala/cn/zhaishidan/exercise/serialization/Main.scala
@@ -0,0 +1,12 @@
+package cn.zhaishidan.exercise.serialization
+
+/**
+  * Created by stan on 2017/1/8.
+  */
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    val task = new SimpleTask()
+    task.run()
+  }
+}
---
+++ src/main/scala/cn/zhaishidan/exercise/serialization/SimpleTask.java
@@ -0,0 +1,12 @@
+package cn.zhaishidan.exercise.serialization;
+
+/**
+ * Created by stan on 2017/1/8.
+ */
+public class SimpleTask extends Task {
+
+    @Override
+    public void run() {
+        System.out.println("run simple task!");
+    }
+}
