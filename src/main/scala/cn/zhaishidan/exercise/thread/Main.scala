787c08b7a233a9d369c1085da53060d079ac7fd0
Stan Zhai <zhaishidan@haizhi.com>
2017-01-17 16:52:46 +0800

    add TimeoutThread

--- src/main/scala/cn/zhaishidan/exercise/thread/Main.scala
+++ src/main/scala/cn/zhaishidan/exercise/thread/Main.scala
@@ -1,11 +1,15 @@
 package cn.zhaishidan.exercise.thread
 
+import java.util.concurrent.Executors
+
 /**
   * Created by stan on 2016/11/15.
   */
 object Main {
 
   def main(args: Array[String]): Unit = {
+    val service = Executors.newCachedThreadPool()
+    service.shutdown()
     println("test")
   }
 
---
+++ src/main/scala/cn/zhaishidan/exercise/thread/TimeoutThread.scala
@@ -0,0 +1,8 @@
+package cn.zhaishidan.exercise.thread
+
+/**
+  * Created by stan on 2016/11/15.
+  */
+class TimeoutThread {
+
+}
